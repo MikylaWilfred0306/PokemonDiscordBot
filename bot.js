ca79c87ec924928bf72c2a3070b2f10ede3d2d65
MikylaWilfred0306 <Mikyla.Wilfred@gmail.com>
2020-11-09 21:09:17 -0500

    Starting API Calls

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
+const Discord = require('discord.io');
+const logger = require('winston');
+const auth = require('./auth.json');
+const axios = require('axios');
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -24,20 +25,30 @@ bot.on('ready', function (evt) {
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to.
-         }
+    if (message.substring(0, 8).toLowerCase() == '!pokemon') {
+        const args = message.substring(1).split(' ');
+        
+        apiCall(args[1].toLowerCase(), channelID, userID, Number(args[1]));     
      }
 });
+
+function apiCall(string, channelID, userID, isNumeric){
+    axios.get('https://pokeapi.co/api/v2/pokemon/' + string)
+        .then(res => {
+            bot.sendMessage({
+                to: channelID,
+                message: (isNumeric ? capitalizeFirstLetter(res.data.name) : res.data.id)  + " <@" + userID + ">",
+            });
+        })
+        .catch(err => {
+            bot.sendMessage({
+                to: channelID,
+                message: err.response.statusText  + " <@" + userID + ">",
+            });
+    });
+}
+
+
+function capitalizeFirstLetter(str){
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
